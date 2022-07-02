35170f3f2a58a34f6b163f62e8ae35322f8d038f
Tharindu Theekshana Dayananda <65506774+venom0039@users.noreply.github.com>
2022-07-02 11:55:37 +0530

    Commit First

---
+++ SalesSystem/Controllers/SaleController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesSystem.Controllers
+{
+    public class SaleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ SalesSystem/Models/SalesLines.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalesApp.Models
+{
+    public class SalesLines
+    {
+        [Key]
+        public int SalesLineID { get; set; }
+        public int SSalesID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double Total { get; set; }
+    }
+}
