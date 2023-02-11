5f6db81c8470804db0d4066b8bc323f313cbabe3
venom0039 <tharindutd1998@gmail.com>
2023-02-11 07:41:37 +0530

    Commit Data

--- POS-DotNET-Core-ReactJS/ClientSide/src/components/Login.tsx
+++ POS-DotNET-Core-ReactJS/ClientSide/src/components/Login.tsx
@@ -82,7 +82,7 @@ export default function Login(){
                                                         <label className="form-label" htmlFor="Password">Password</label>
                                                     </div>
                                                     <div className="text-center pt-1 mb-5 pb-1">
-                                                        <button className="btn login-gradient-custom-2 mb-3 px-5 text-light" type="submit">Login</button>
+                                                        <button className="btn login-gradient-custom-2 mb-3 px-5 text-light" onClick={() => login()} type="button">Login</button>
                                                         {/*<a className="text-muted" href="#!">Forgot password?</a>*/}
                                                     </div>
                                                 </div>
